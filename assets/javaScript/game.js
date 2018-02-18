c5eb0df31d918b2cedac7e4173ec517ef38dcba1
unknown <ejzagala@gmail.com>
2018-02-17 17:13:44 -0700

    Replacing psuedo-code in methods

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -1,11 +1,19 @@
 var hangman = {
     Wins: 0, 
     remGuesses: 15, 
-    words: [],
+    words: [1, 2, 3],
     currentWord: [], 
     lettersGuessed: [], 
-    matchKey: function(x) {
+    wordBank: ["lynch", "noose","hangman", "dangling", "executioner", "gallows", "outlaw", "gunslinger"], 
+    initialize: function(x){
+        hangman.wins = 0;
+        hangman.remGuesses = 15; 
+        hangman.currentWord = hangman.wordBank[Math.floor(Math.random() * hangman.wordBank.length)];
+        hangman.lettersGuessed = hangman.lettersGuessed.splice(0, hangman.lettersGuessed.length);
+    },
+    matchKey: function(arr) {
         // method that matches key pressed w/ letter in curent word
+
     }, 
     appendToLG: function(x) {
         // method that pushes key pressed into lettersGuessed if it's not matched to the word 
@@ -19,3 +27,15 @@ var hangman = {
         remGuesses--; 
     }
 }
+
+if (document.getElementById("startedContainer").style.display !== "none") {
+    document.onkeypress = function(evt){
+        hangman.initialize();
+        document.getElementById("startedContainer").style.display = "none";
+        document.getElementById("wins").innerHTML = ' ' + hangman.wins; 
+        document.getElementById("remGuesses").innerHTML = ' ' + hangman.remGuesses;    
+    }
+}
+
+
+
