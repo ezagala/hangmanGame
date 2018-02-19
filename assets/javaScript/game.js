6393deb12e5c85e784f66da65d17b92a34eb0146
unknown <ejzagala@gmail.com>
2018-02-18 22:19:40 -0700

    Progress made on gampePlay

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -1,12 +1,24 @@
 var hangman = {
+    wordBank: ["lynch", "noose","hangman", "dangling", "executioner", "gallows", "outlaw", "gunslinger"],
     Wins: 0, 
     remGuesses: 15, 
+    userInput: [], 
     currentWord: [], 
     currentGuess: [], 
     lettersGuessed: [], 
-    wordBank: ["lynch", "noose","hangman", "dangling", "executioner", "gallows", "outlaw", "gunslinger"], 
-    initialize: function(x){
-        //placeHolder function generates the sequence of underscores that correspond to the missing letters 
+    initialize: function(x) {
+        document.onkeypress = function(event){
+            if (document.getElementById("startedContainer").style.visibility !== "hidden" || this.remGuesses === 0) {
+                document.getElementById("startedContainer").style.visibility = "hidden";
+                this.wins = 0;
+                document.getElementById("wins").innerHTML = '  ' + this.wins;
+                this.remGuesses = 15; 
+                document.getElementById("remGuesses").innerHTML = '  ' + this.remGuesses;    
+            }
+        }
+    },
+    gamePlay: function() {
+
         var placeHolder = function(arr) {
             for (var i = 0; i < arr.length; i++){
                 hangman.currentGuess.push("__ "); 
@@ -14,50 +26,67 @@ var hangman = {
             return hangman.currentGuess; 
         }
 
-        //If the targeted container is on the page or remGuesses is 0: (1) hide the startedContainer elem., (2) set and render wins, (3) set and render remaining guesses, (4) set a current word and render a string of underscores that matches the length of the current word, (5) reset letters guessed
-        document.onkeypress = function(event){
-            if (document.getElementById("startedContainer").style.display !== "none" || this.remGuesses === 0) {
-                document.getElementById("startedContainer").style.display = "none";
-                this.wins = 0;
-                document.getElementById("wins").innerHTML = '  ' + this.wins;
-                this.remGuesses = 15; 
-                document.getElementById("remGuesses").innerHTML = '  ' + this.remGuesses;    
-                this.currentWord = hangman.wordBank[Math.floor(Math.random() * hangman.wordBank.length)];
-                document.getElementById("currWord").innerHTML = '  ' + placeHolder(this.currentWord).toString(); 
-                this.lettersGuessed = hangman.lettersGuessed.splice(0, hangman.lettersGuessed.length);
-            }
-        }
-    },
-    gamePlay: function(x) {
+        this.currentWord = hangman.wordBank[Math.floor(Math.random() * hangman.wordBank.length)].split('');
+        document.getElementById("currWord").innerHTML = '  ' + placeHolder(this.currentWord).toString().replace(/,/g, '');
+        this.lettersGuessed = hangman.lettersGuessed.splice(0, hangman.lettersGuessed.length);
+
+        console.log(hangman.currentWord)
+
         var matchKey = function(arr) {
-            // If input matches a letter in currentWord: (1) replace the underscore with the guessed letter at its index
-            document.onkeypress = function(event){
-                var input = event.key; 
+            document.onkeydown = function(event){
+            hangman.userInput.push(event.key);
+            //TEST ZONE BEGIN
+            // console.log(hangman.userInput[hangman.userInput.length - 1])
+            // TEST ZONE END    
+                for (var i = 0; i < arr.length; i++) {
+                    if (arr[i] === hangman.userInput[hangman.userInput.length - 1]) {
+                        hangman.currentGuess[i] = hangman.userInput[hangman.userInput.length - 1]; 
+                        console.log(hangman.currentGuess)
+                        document.getElementById("currWord").innerHTML = '  ' + hangman.currentGuess.toString().replace(/,/g, ''); 
+                    }
+                }
                 
             }
+        }
+        matchKey(hangman.currentWord); 
+        // function appendToLG() {
+        //     // method that pushes key pressed into lettersGuessed if it's not matched to the word 
+        // }
+        
+        // incWins(hangman.currentGuess)
 
-        }
-        var appendToLG = function(x) {
-            // method that pushes key pressed into lettersGuessed if it's not matched to the word 
-        }
-        var incWins = function(x) {
-            // Increment wins if the current word is equal to the current guess
-            if (this.currentGuess === this.currentWord) {
-                Wins++;    
-            }
-        }
-        var decRemGuesses = function(x) {
-            // if the letter isn't matched... 
-            remGuesses--; 
-        }
+        // var decRemGuesses = function(arr) {
+        //     document.onkeydown = function(event){
+        //         hangman.userInput.push(event.key);   
+        //             for (var i = 0; i < arr.length; i++) {
+        //                 if (arr[i] !== hangman.userInput[hangman.userInput.length - 1]) {
+        //                      hangman.remGuesses-- 
+        //                      break; 
+        //                     console.log(hangman.remGuesses)
+        //                 }
+        //             }
+        //         }
+        //     }
+        // decRemGuesses(hangman.currentWord); 
     }
 }
 
+// var incWins = function (arr) {
+//     // Increment wins if the current word is equal to the current guess
+//     if (arr == hangman.currentWord) {
+//         Wins++;    
+//         console.log(hangman.Wins)
+//     }
+// }
+
+
+
 
 hangman.initialize(); 
-console.log(typeof hangman.currentGuess)
-// hangman.gamePlay(); 
+hangman.gamePlay(); 
 
+
+//console.log(hangman.userInput[hangman.userInput.length - 1])
 
 
 
