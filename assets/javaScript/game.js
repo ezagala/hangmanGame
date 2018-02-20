ee98536058d469b70da48a411bfd96de6ee903d7
unknown <ejzagala@gmail.com>
2018-02-19 23:59:07 -0700

    Refactored, but still not there.

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -7,92 +7,84 @@ var hangman = {
     currentGuess: [], 
     lettersGuessed: [], 
     initialize: function() {
-        document.onkeypress = function(event){
-            if (document.getElementById("startedContainer").style.visibility !== "hidden" || this.remGuesses === 0) {
-                document.getElementById("startedContainer").style.visibility = "hidden";
-                this.wins = 0;
-                document.getElementById("wins").innerHTML = '  ' + this.wins;
-                this.remGuesses = 15; 
-                document.getElementById("remGuesses").innerHTML = '  ' + this.remGuesses;    
+        if (document.getElementById("startedContainer").style.visibility !== "hidden" || hangman.remGuesses === 0) {
+            document.getElementById("startedContainer").style.visibility = "hidden";
+            this.wins = 0;
+            document.getElementById("wins").innerHTML = '  ' + this.wins;
+            this.remGuesses = 15; 
+            document.getElementById("remGuesses").innerHTML = '  ' + this.remGuesses;    
+
+            var placeHolder = function(arr) {
+                for (var i = 0; i < arr.length; i++){
+                    hangman.currentGuess.push("__ "); 
+                }
+                return hangman.currentGuess; 
             }
+    
+            hangman.currentWord = hangman.wordBank[Math.floor(Math.random() * hangman.wordBank.length)].split('');
+            
+            document.getElementById("currWord").innerHTML = '  ' + placeHolder(hangman.currentWord).toString().replace(/,/g, '');
         }
     },
     gamePlay: function() {
 
-        var placeHolder = function(arr) {
-            for (var i = 0; i < arr.length; i++){
-                hangman.currentGuess.push("__ "); 
-            }
-            return hangman.currentGuess; 
-        }
-
-        document.getElementById("currWord").innerHTML = '  ' + placeHolder(this.currentWord).toString().replace(/,/g, '');
-
-        this.currentWord = hangman.wordBank[Math.floor(Math.random() * hangman.wordBank.length)].split('');
-        
         console.log(hangman.currentWord)
+        console.log(hangman.userInput[hangman.userInput.length - 1])
+        console.log(hangman.currentWord[0]) 
+        
 
-        document.onkeydown = function(event){
-        hangman.userInput.push(event.key);
-
-            var matchKey = function(arr) {
-                for (var i = 0; i < arr.length; i++) {
-                    if (arr[i] === hangman.userInput[hangman.userInput.length - 1]) {
-                        var matchInput = hangman.userInput;
-                        hangman.currentGuess[i] = matchInput[matchInput.length - 1]; 
-                        console.log(hangman.currentGuess)
-                        document.getElementById("currWord").innerHTML = '  ' + hangman.currentGuess.toString().replace(/,/g, ''); 
-                    } 
-                }
+        var matchKey = function(arr) {
+            hangman.currentGuess[i] = hangman.userInput[hangman.userInput.length - 1]; 
+            document.getElementById("currWord").innerHTML = '  ' + hangman.currentGuess.toString().replace(/,/g, '');
             }
             
-            matchKey(hangman.currentWord); 
+        var appendToLG = function(arr) {
+            hangman.lettersGuessed.push(hangman.userInput[hangman.userInput.length - 1])
+            // console.log(hangman.lettersGuessed)
+            document.getElementById("lettersGuessed").innerHTML = '  ' + hangman.lettersGuessed.toString();
+        }
 
-            var appendToLG = function(arr) {
-                for (var i = 0; i < arr.length; i++) {
-                    if (arr[i] !== hangman.userInput[hangman.userInput.length - 1]) {
-                        var appendInput = hangman.userInput;
-                        hangman.lettersGuessed.push(hangman.userInput[hangman.userInput.length - 1])
-                        console.log(hangman.lettersGuessed)
-                        document.getElementById("lettersGuessed").innerHTML = '  ' + hangman.lettersGuessed.toString(); 
-                        break; 
-                    } 
-                }
-            }
-            appendToLG(hangman.currentGuess); 
+        var decRemGuesses = function(arr) {
+            hangman.remGuesses--  
+            // console.log(hangman.remGuesses)
+            document.getElementById("remGuesses").innerHTML = '  ' + hangman.remGuesses;
+        }
 
-            var decRemGuesses = function(arr) {
-                var decInput = hangman.userInput; 
-                for (var i = 0; i < arr.length; i++) {
-                    if (arr[i] !== decInput[decInput - 1]) {
-                        hangman.remGuesses--  
-                        console.log(hangman.remGuesses)
-                        document.getElementById("remGuesses").innerHTML = '  ' + hangman.remGuesses;
-                        break; 
-                    }
-                }
-                
-            }
-            decRemGuesses(hangman.currentGuess); 
+        var incWins = function (arr) {
+            hangman.Wins++  
+            document.getElementById("wins").innerHTML = '  ' + hangman.Wins;             
+        }
+        // incWins(hangman.currentGuess)  
+        
+        for (var i = 0; i < hangman.currentWord.length; i++) {
+            console.log(hangman.currentWord[i] === hangman.userInput[hangman.userInput.length - 1])
+            if (hangman.currentWord[i] === hangman.userInput[hangman.userInput.length - 1]) {
+                matchKey(hangman.currentWord); 
+            }         
+        }  
 
-            var incWins = function (arr) {
-                var incInput = hangman.userInput; 
-                for (var i = 0; i < arr.length; i++) {
-                    if (arr[i] != incInput[incInput.length - 1]) {
-                        hangman.Wins++  
-                        console.log(hangman.Wins)
-                        document.getElementById("wins").innerHTML = '  ' + hangman.Wins; 
-                        break; 
-                    }
-                }
-            }
-            incWins(hangman.currentGuess)
-        }    
+        for (var i = 0; i < hangman.currentWord.length; i++) {
+            console.log(hangman.currentWord[i] !== hangman.userInput[hangman.userInput.length - 1])
+            if (hangman.currentWord[i] !== hangman.userInput[hangman.userInput.length - 1]) {
+                appendToLG(hangman.currentGuess);
+                decRemGuesses(hangman.currentGuess);  
+                break;  
+            } 
+            break; 
+        }
     }
 }
 
-hangman.initialize(); 
-hangman.gamePlay(); 
+
+
+document.onkeypress = function(event){
+    hangman.userInput.push(event.key);
+    hangman.initialize();   
+    hangman.gamePlay();
+}
+
+
+
 
 
 
